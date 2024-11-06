eada19212a6a168014e2921e4a228d5467c81bf0
CarlosPerez <carlosmperez2901@gmail.com>
2024-11-05 22:39:54 -0800

    Added result.html page as the result html page. Also Finished all the length convertion. Models implemented as well

--- back-end/converter/models.py
+++ back-end/converter/models.py
@@ -1,3 +1,226 @@
-from django.db import models
+from abc import ABC, abstractmethod
 
-# Create your models here.
+
+class ILengthMeasure(ABC):
+
+    @abstractmethod
+    def meter(self): ...
+
+    @abstractmethod
+    def kilometer(self): ...
+
+    @abstractmethod
+    def centimeter(self): ...
+
+    @abstractmethod
+    def mile(self): ...
+
+    @abstractmethod
+    def inch(self): ...
+
+    @abstractmethod
+    def foot(self): ...
+
+
+class Inch(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number / 39.37
+
+    @property
+    def kilometer(self):
+        return self.number / 39370
+
+    @property
+    def centimeter(self):
+        return self.number * 2.54
+
+    @property
+    def inch(self):
+        return self.number
+
+    @property
+    def foot(self):
+        return self.number / 12
+
+    @property
+    def mile(self):
+        return self.number / 63360
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Foot(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number * 0.3048
+
+    @property
+    def kilometer(self):
+        return self.number * 0.0003048
+
+    @property
+    def centimeter(self):
+        return self.number * 30.48
+
+    @property
+    def mile(self):
+        return self.number * 0.000189394
+
+    @property
+    def inch(self):
+        return self.number * 12
+
+    @property
+    def foot(self):
+        return self.number
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Mile(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number * 1609.34
+
+    @property
+    def kilometer(self):
+        return self.number * 1.60934
+
+    @property
+    def centimeter(self):
+        return self.number * 160934
+
+    @property
+    def foot(self):
+        return self.number * 5280
+
+    @property
+    def inch(self):
+        return self.number * 63360
+
+    @property
+    def mile(self):
+        return self.number
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Meter(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number
+
+    @property
+    def kilometer(self):
+        return self.number / 1000
+
+    @property
+    def centimeter(self):
+        return self.number * 100
+
+    @property
+    def foot(self):
+        return self.number * 3.281
+
+    @property
+    def inch(self):
+        return self.number * 39.37
+
+    @property
+    def mile(self):
+        return self.number / 1609
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Kilometer(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number * 1000
+
+    @property
+    def kilometer(self):
+        return self.number
+
+    @property
+    def centimeter(self):
+        return self.number * 100000
+
+    @property
+    def foot(self):
+        return self.number * 3281
+
+    @property
+    def inch(self):
+        return self.number * 39370
+
+    @property
+    def mile(self):
+        return self.number / 1.609
+
+    def __str__(self):
+        return str(self.number)
+
+
+class Centimeter(ILengthMeasure):
+    number: float
+
+    def __init__(self, number: float):
+        self.number = float(number)
+
+    @property
+    def meter(self):
+        return self.number / 100
+
+    @property
+    def kilometer(self):
+        return self.number / 100000
+
+    @property
+    def centimeter(self):
+        return self.number * 100000
+
+    @property
+    def foot(self):
+        return self.number * 3281
+
+    @property
+    def inch(self):
+        return self.number * 39370
+
+    @property
+    def mile(self):
+        return self.number / 1.609
+
+    def __str__(self):
+        return str(self.number)
--- back-end/converter/views.py
+++ back-end/converter/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from converter.models import (
+    ILengthMeasure,
+    Foot,
+    Mile,
+    Inch,
+    Meter,
+    Kilometer,
+    Centimeter,
+)
 
 
 def length_page(request):
+    print("wiii", request.POST)
+    if request.method == "POST":
+        return lenght_converter(request)
+
     return render(request, "length-page.html")
 
 
@@ -11,3 +24,46 @@ def weight_page(request):
 
 def temperature_page(request):
     return render(request, "temperature-page.html")
+
+
+def lenght_converter(request):
+    convert_from: str = request.POST.get("convert_from")
+    convert_to: str = request.POST.get("convert_to")
+    param_number = request.POST.get("number")
+    number: ILengthMeasure
+    number_result: float = 0
+
+    if convert_from.lower() == "mile":
+        number = Mile(param_number)
+    elif convert_from.lower() == "foot":
+        number = Foot(param_number)
+    elif convert_from.lower() == "meter":
+        number = Meter(param_number)
+    elif convert_from.lower() == "kilometer":
+        number = Kilometer(param_number)
+    elif convert_from.lower() == "centimeter":
+        number = Centimeter(param_number)
+    elif convert_from.lower() == "inch":
+        number = Inch(param_number)
+
+    if convert_to.lower() == "mile":
+        number_result = number.mile
+    elif convert_to.lower() == "foot":
+        number_result = number.foot
+    elif convert_to.lower() == "meter":
+        number_result = number.meter
+    elif convert_to.lower() == "kilometer":
+        number_result = number.kilometer
+    elif convert_to.lower() == "centimeter":
+        number_result = number.centimeter
+    elif convert_to.lower() == "inch":
+        number_result = number.inch
+
+    context = {
+        "convert_from": convert_from,
+        "convert_to": convert_to,
+        "unit": number,
+        "number_result": number_result,
+    }
+
+    return render(request, "results.html", context)
